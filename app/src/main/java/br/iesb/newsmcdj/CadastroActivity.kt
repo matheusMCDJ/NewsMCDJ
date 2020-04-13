e6295aa61eaf0633d961d1037cd6235cd615eb80
matheusMCDJ <50184378+matheusMCDJ@users.noreply.github.com>
2020-04-13 16:23:17 -0300

    Criação primária das telas de cadastro, login e recupera senha.

---
+++ app/src/main/java/br/iesb/newsmcdj/CadastroActivity.kt
@@ -0,0 +1,12 @@
+package br.iesb.newsmcdj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
---
+++ app/src/main/java/br/iesb/newsmcdj/LoginActivity.kt
@@ -0,0 +1,12 @@
+package br.iesb.newsmcdj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
